b0f52d2a8d378577e1e398a8aa67461bf2d83d45
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2018-10-13 14:18:01 -0700

    Move httpsproxy to its own repo

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/fishy/blynk-proxy
+
+require github.com/fishy/httpsproxy v0.0.0-20181013211500-c0863020eb09
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/fishy/blynk-proxy/lib/httpsproxy"
+	"github.com/fishy/httpsproxy"
 )
 
 const (
